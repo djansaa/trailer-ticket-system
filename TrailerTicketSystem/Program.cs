835d0ed6581e6e1ad5e0152c9a7c8f7a2df487fe
djansaa <103861560+djansaa@users.noreply.github.com>
2025-11-21 11:41:56 +0100

    Convert Razor Pages to MVC views

--- TrailerTicketSystem/Program.cs
+++ TrailerTicketSystem/Program.cs
@@ -5,7 +5,6 @@ using TrailerTicketSystem.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 
 // load connection string from user-secrets
@@ -20,12 +19,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseRouting();
 
@@ -34,9 +35,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
 
 app.Run();
